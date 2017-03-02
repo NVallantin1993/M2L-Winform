8655151b8ce3cdd54eea857dcce69633c441c558
NVallantin1993 <asdeath@hotmail.fr>
2017-03-02 10:05:43 +0100

    Commit Initial

---
+++ M2L-ProjetWinform/ListeAllAdherents.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace M2L_ProjetWinform
+{
+    partial class ListeAllAdherents
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvListAdhe = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvListAdhe
+            // 
+            this.lvListAdhe.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.lvListAdhe.Location = new System.Drawing.Point(12, 12);
+            this.lvListAdhe.Name = "lvListAdhe";
+            this.lvListAdhe.Size = new System.Drawing.Size(1121, 432);
+            this.lvListAdhe.TabIndex = 0;
+            this.lvListAdhe.UseCompatibleStateImageBehavior = false;
+            this.lvListAdhe.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "nom";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Prenom";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Sexe";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "date de naissance";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Rue adresse";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Code postal";
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Ville";
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Cotisation";
+            // 
+            // ListeAllAdherents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 547);
+            this.Controls.Add(this.lvListAdhe);
+            this.Name = "ListeAllAdherents";
+            this.Text = "Liste de tout les adhérents";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.ListeAllAdherents_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvListAdhe;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+    }
+}
